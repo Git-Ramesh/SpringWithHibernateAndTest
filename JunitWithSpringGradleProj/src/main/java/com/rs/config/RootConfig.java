1f11284a8ab0d6eadd6c53aad3c47174765e0a48
Ramesh <rameshpanthangi08@gmail.com>
2018-02-17 02:35:48 +0530

    Login Code Completed

--- JunitWithSpringGradleProj/src/main/java/com/rs/config/RootConfig.java
+++ JunitWithSpringGradleProj/src/main/java/com/rs/config/RootConfig.java
@@ -2,9 +2,11 @@ package com.rs.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages= {"com.rs.service","com.rs.dao"})
+@ComponentScan(basePackages= {"com.rs.service"})
+@Import(value= {PersistenceConfig.class})
 public class RootConfig {
 	public RootConfig() {
 		System.out.println("RootConfig: 0-param constr");
--- JunitWithSpringGradleProj/src/main/java/com/rs/dao/LoginDAOImpl.java
+++ JunitWithSpringGradleProj/src/main/java/com/rs/dao/LoginDAOImpl.java
@@ -1,18 +1,41 @@
 package com.rs.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Types;
+
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository(value="loginDAO")
 public class LoginDAOImpl implements LoginDAO {
-
+	@Autowired
+	HibernateTemplate ht;
 	
 	@Override
 	public int checkLogin(String username, String password) {
+		String QUERY="{call P_LOGIN_CHECK(?,?,?)}";
+		Session session=null;
 		System.out.println(username+" "+password);
-		if(username.equals("Ramesh")&&password.equals("1234")) {
-			return 1;
-		}
-		return 0;
+		int count=0;
+		
+		ht.setCheckWriteOperations(false);
+		session=ht.getSessionFactory().openSession();
+		count=session.doReturningWork(conn->{
+			CallableStatement cstmt=null;
+			int flag=0;
+			cstmt=conn.prepareCall(QUERY);
+			cstmt.setString(1,username);
+			cstmt.setString(2,password);
+			cstmt.registerOutParameter(3,Types.INTEGER);
+			cstmt.execute();
+			flag=cstmt.getInt(3);
+			
+			return flag;
+		});
+		System.out.println("Count: "+count);
+		return count;
 	}
 
 }
